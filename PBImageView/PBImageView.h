ca9d0099a7b4fb7ab59f567aee308bdcfe93725d
Povilas Juzeliunas <pju@trifork.com>
2020-01-17 14:20:12 +0100

    Framework project and shared scheme added

---
+++ PBImageView/PBImageView.h
@@ -0,0 +1,19 @@
+//
+//  PBImageView.h
+//  PBImageView
+//
+//  Created by Povilas Juzeliunas on 17/01/2020.
+//  Copyright © 2020 Povilas Juzeliunas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PBImageView.
+FOUNDATION_EXPORT double PBImageViewVersionNumber;
+
+//! Project version string for PBImageView.
+FOUNDATION_EXPORT const unsigned char PBImageViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PBImageView/PublicHeader.h>
+
+
